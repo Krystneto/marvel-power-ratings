79e237c649724387ffcbd1f939f7ee11c7ad9741
Kristopher Parchen <kristopherparchen@MacBook-Pro-4.local>
2019-01-04 17:27:01 -0800

    adds .env

--- express/api/controllers/characters.js
+++ express/api/controllers/characters.js
@@ -1,17 +1,19 @@
 "use strict"
 const _ = require("lodash/core");
 
-const data = require('../../../mock-heroes.json');
+require('dotenv').config()
 const charSchema = require('../../../schema/characters-schema.json')
 
 
 module.exports.getCharacters = (req, res) => {
+
     let { query } = req;
     let { name } = req.query;
+    let data = JSON.parse(process.env.HEROES);
     let { characters } = data;
 
     if (name) {
-        let character = _.filter(charData.characters, el => el.name.toLowerCase() === name.toLowerCase());
+        let character = _.filter(characters, el => el.name.toLowerCase() === name.toLowerCase());
         res.send(character);
     } else if (query) {
         let charsList = _.filter(characters, { "attributes": query })
@@ -23,6 +25,7 @@ module.exports.getCharacters = (req, res) => {
 
 module.exports.getCharacterById = (req, res) => {
     let { id } = req.params;
+    let data = JSON.parse(process.env.HEROES);
     let { characters } = data;
     
     let char = _.filter(characters, el => el.id === id);

bf237b19a3e1314faf7de1909b4619c5ba5b2047
Kristopher Parrillo <kkparrillo@gmail.com>
2019-01-05 21:37:46 -0800

    updates to response with .json

--- express/api/controllers/characters.js
+++ express/api/controllers/characters.js
@@ -2,38 +2,38 @@
 require('dotenv').config();
 const _ = require("lodash/core");
 
-const charSchema = require('../../../schema/characters-schema.json')
-const data = JSON.parse(process.env.CHARACTERS) || require('../../../characters.json')
 
 module.exports.getCharacters = (req, res) => {
+    const data = JSON.parse(process.env.CHARACTERS) || require('../../../characters.json')
     // console.log(process.env)
     // console.log(JSON.parse(process.env.CHARACTERS))
     // const data = process.env.CHARACTERS //|| require('../../../characters.json')
     let { query } = req;
     let { name } = req.query;
     let { characters } = data;
-    console.log(characters)
+    // console.log(characters)
 
     if (name) {
         let character = _.filter(characters, el => el.name.toLowerCase() === name.toLowerCase());
-        res.send(character);
+        res.json(character);
     } else if (query) {
         let charsList = _.filter(characters, { "attributes": query })
-        res.send(charsList)
+        res.json(charsList)
     } else {
-        res.send(data);
+        res.json(data);
     }
 }
 
 module.exports.getCharacterById = (req, res) => {
-    // const data = process.env.CHARACTERS || require('../../../characters.json')
+    const data = JSON.parse(process.env.CHARACTERS) || require('../../../characters.json')
     let { id } = req.params;
     let { characters } = data;
     
     let char = _.filter(characters, el => el.id === id);
-    res.send(char)
+    res.json(char)
 }
 
 module.exports.getCharactersSchema = (req, res) => {
-    res.send(charSchema)
+    const charSchema = require('../../../schema/characters-schema.json')
+    res.json(charSchema)
 }
